007a1c41c7ad9bc0536302ed7993a19602f5c01b
lamstroning <lexashob@yandex.ru>
2021-05-31 23:17:18 +0300

    project base

---
+++ src/components/RightPanel/RightPanel.js
@@ -0,0 +1,11 @@
+import Menu from '../Menu/Menu';
+import SearchRow from '../SearchRow';
+
+export default function RightPanel() {
+    return (
+        <>
+            <SearchRow/>
+            <Menu/>
+        </>
+    )
+}
